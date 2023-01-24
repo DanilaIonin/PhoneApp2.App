8ea6380c1f7e440b00030c68155db5690f28a97a
Админ <@DESKTOP-H16T94L>
2023-01-24 11:31:17 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using PhoneApp2.Views;
+namespace PhoneApp2;
+
+public partial class App : Application
+{
+    public App()
+    {
+        MainPage = new NavigationPage(new FriendsListPage());
+    }
+
+    protected override void OnStart()
+    { }
+
+    protected override void OnSleep()
+    { }
+
+    protected override void OnResume()
+    { }
+}
